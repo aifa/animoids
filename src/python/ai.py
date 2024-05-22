362567c6bb95bfdd8d72fe99724385f27aba80f6
aifa <antonis@typesystem.xyz>
2024-05-22 13:36:28 +0200

    prompt+openai call

--- src/python/ai.py
+++ src/python/ai.py
@@ -1,25 +1,71 @@
-import os
-import langchain
-import BaseModel, Field
+from openai import OpenAI
+import yaml
 from dotenv import load_dotenv
-
+import os
+import requests
+import json
 
 load_dotenv()
 
 open_ai_key = os.getenv("OPENAI_API_KEY")
 
-def load_image(inputs: dict) -> dict:
-    """Load image from file and encode it as base64."""
-    image_path = inputs["image_path"]
-  
-    def encode_image(image_path):
-        with open(image_path, "rb") as image_file:
-            return base64.b64encode(image_file.read()).decode('utf-8')
-    image_base64 = encode_image(image_path)
-    return {"image": image_base64}
+# Function to load and fill the prompt template from a YAML file
+def load_prompt_template(yaml_path, **kwargs):
+    with open(yaml_path, 'r') as file:
+        data = yaml.safe_load(file)
+    template = data['prompt']
+    return template.format(**kwargs)
 
-load_image_chain = TransformChain(
-    input_variables=["image_path"],
-    output_variables=["image"],
-    transform=load_image
-)
+
+def submit_request(ipfs_url):
+    yaml_path = 'prompt.yaml'
+    prompt = load_prompt_template(yaml_path)
+
+    client = OpenAI()
+
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {open_ai_key}"
+    }
+
+    payload = {
+    "model": "gpt-4o",
+    "response_format": { "type": "json_object" },
+    "messages": [
+        {
+        "role": "user",
+        "content": [
+                {
+                    "type": "text",
+                    "text": f"{prompt}"
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"{ipfs_url}"
+                    }
+                }
+            ]
+        }
+    ],
+    "max_tokens": 1000
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    content = response.json()
+
+    ai_response = content["choices"][0]["message"]["content"]
+    #summary = content["choices"][0]["message"]["content"]["summary"]
+    #result = content["choices"][0]["message"]["content"]["result"]
+    print(ai_response)
+    #print(summary)
+    #print(result)
+    return ai_response
+
+
+
+
+if __name__ == "__main__":
+    ipfs_url = "https://bafybeiewtbig2gv4hzgwtnbdwozk66m5xs5qfgorzutuff34xfmu7kr3jm.ipfs.w3s.link/Girl-Isreal-Gaza-Conflict.jpg"
+    submit_request(ipfs_url)
+    
