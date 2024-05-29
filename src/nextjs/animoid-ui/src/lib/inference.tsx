cadba3ec2fdf31b8f5b50e90ba7b2c8746d39771
aifa <antonis@typesystem.xyz>
2024-05-29 20:04:04 +0200

    removed typo and console.log

--- src/nextjs/animoid-ui/src/lib/inference.tsx
+++ src/nextjs/animoid-ui/src/lib/inference.tsx
@@ -1,11 +1,5 @@
 export const runLlavaInference = async (imageFolderCid: string) => {
 
-    console.log(JSON.stringify({
-        "pk": `${process.env.NEXT_PRIVATE_LILYPAD_KEYs}`,
-        "module": "github.com/aifa/lilypad-module-ollama-pipeline:v0.0.11",
-        "inputs": "-i Prompt='Please describe the objects and people in this image. On a scale from 0 to 1, can you tell if this has been artificially modified or generated?' -i imgCid="+`${imageFolderCid}`,
-         "opts": { "stream": false}
-    }));
     const response = await fetch(`http://js-cli-wrapper.lilypad.tech`,
         {
             method: 'POST',
